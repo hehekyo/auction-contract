39a53cf5fd5f6c84377fd7f71e3cdc6593f43210
592363789 <592363789@qq.com>
2024-11-14 18:05:25 +0800

    ipfs: tokenURI

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -3,27 +3,32 @@ pragma solidity ^0.8.22;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract MyNFT is ERC721, Ownable {
+    using Strings for uint256;
     uint256 public tokenCounter;
-    string public _baseTokenURI;
+    mapping(uint256 => string) public _tokenURIs;
 
     function _exists(uint256 tokenId) internal view returns (bool) {
         return _ownerOf(tokenId) != address(0);
     }
 
-    constructor(address owner, string memory baseURI) Ownable(owner) ERC721("AuctionNFT", "ANFT") {
+    constructor(address owner) Ownable(owner) ERC721("AuctionNFT", "ANFT") {
         tokenCounter = 0;
-        _baseTokenURI = baseURI;
-
     }
 
-    function tokenURI(uint256 tokenId) public view override onlyOwner returns (string memory) {
-        return super.tokenURI(tokenId);
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        require(_exists(tokenId), "Token does not exist");
+        string memory _tokenURI = _tokenURIs[tokenId];
+        require(bytes(_tokenURI).length > 0, "Token URI not set");
+        return _tokenURI;
     }
 
-    function _baseURI() internal view virtual override returns (string memory)  {
-        return _baseTokenURI;
+
+    function setTokenURI(uint256 tokenId, string memory _cid) public onlyOwner {
+        require(_exists(tokenId), "Token does not exist");
+        _tokenURIs[tokenId] = string(abi.encodePacked("ipfs://", _cid, "/", tokenId.toString(), ".json"));
     }
 
     function mint() public onlyOwner {
